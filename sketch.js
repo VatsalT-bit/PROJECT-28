a1ef9bb8a510a0d768c5e676072ec32c7480b9a4
VatsalT-bit <76956307+VatsalT-bit@users.noreply.github.com>
2021-04-25 18:31:53 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -8,9 +8,13 @@ const Constraint=Matter.Constraint;
 var ground;
 var tree;
 var boy,boyimage;
+var mango1,mango2,mango3,mango4,mango5,mango6,mango7,mango8,mango9;
+var stone;
+var launcherobject;
+var launchforce=100;
 
 function preload(){
-	boyimage=loadImage("boy.png");
+	boyimage=loadImage("images/boy.png");
 }
 
 function setup() {
@@ -19,15 +23,28 @@ function setup() {
 	world = engine.world;
   
 	ground = new Ground(650,589,1298,10);
-	tree   = new Tree(500,650,20,10);
-	 
-	boy.createSprite(400,200,10,20);
-    boy.addImage("boyimage");
+  tree= new Tree (1000,335,500,495);
+	mango1= new Mango (830,260,80,10);
+  mango2= new Mango (890,270,70,10);
+	mango3= new Mango (950,280,80,10);
+	mango4= new Mango (940,180,70,10);
+	mango5= new Mango (1000,180,70,10);
+	mango6= new Mango (1080,180,80,10);
+	mango7= new Mango (1080,280,60,10);
+	mango8= new Mango (1210,282,70,10);
+	mango9= new Mango (1141,282,70,10);
+
+  stone= new Stone (220,450,90,20);
+  stone.scale=0.5;
+
+	boy=createSprite(300,500,10,20);
+  boy.addImage(boyimage);
+  boy.scale=0.1;
 
 
   //create launcherObject here
 
- 
+  launcherobject= new Launcher (stone.body,{x:235,y:420});
    
 	Engine.run(engine);
 }
@@ -36,18 +53,37 @@ function draw() {
 
   background(230);
   drawSprites();
+
+  createEdgeSprites();
+  tree.collide=ground;
   
   ground.display();
   tree.display();
+  mango1.display();
+  mango2.display();
+  mango3.display();
+  mango4.display();
+  mango5.display();
+  mango6.display();
+  mango7.display();
+  mango8.display();
+  mango9.display();
+
+  stone.display();
+
+  launcherobject.display();
 }
 
-//create mouseDragged function here
+function mouseDragged(){
+  Matter.Body.setPosition(launcherobject.body, {x: mouseX , y: mouseY});
 
+}
 
-//create mouseReleased function here
+function mouseReleased(){
+  Launcher.fly();
 
+}
 
-//create keyPressed function here
 
 
   
